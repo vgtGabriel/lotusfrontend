442ed14e0b9593b504bdb89fca98af837578dc9d
vgtGabriel <gabriel.perfil.pessoal@gmail.com>
2019-06-02 12:20:18 -0300

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from './App';
 
 import "assets/css/black-dashboard-react.css";
-import "assets/demo/demo.css";
 import "assets/css/nucleo-icons.css";
 
 ReactDOM.render(<App/>, document.getElementById('root'));
