0fc8a0e60b674593535c004cd6d2f652eea48989
vgGabriel <gabriel.perfil.pessoal@gmail.com>
2019-07-29 09:45:30 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL:process.env.REACT_APP_API_URL || 'http://localhost:3333',
+    baseURL:process.env.REACT_APP_API_URL
 });
-// api.addResponseTransform(Response =>{
+// || 'http://localhost:3333' api.addResponseTransform(Response =>{
 //     if(!Response.ok) throw Response;
 // });
 export default api;
